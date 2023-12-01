b13dc852b3c728052a000c390b1e304c74f5b1fb
justin <justinkgibbons@gmail.com>
2023-11-30 17:04:02 -0800

    remove ardb dependency

--- src/arweave/arweave.ts
+++ src/arweave/arweave.ts
@@ -1,10 +1,7 @@
 import Arweave from 'arweave';
-import ArDB from 'ardb';
 
 export const arweave = Arweave.init({
 	host: 'arweave.net',
 	port: 443,
 	protocol: 'https'
 });
-
-export const ardb = new ArDB(arweave);
